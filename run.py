6935f5d9685656861855341707eaa751b298e1fa
zhuyuhan2333 <zhuyuhan2333@qq.com>
2025-05-04 13:32:50 +0800

    feat: compare with qwen

--- run.py
+++ run.py
@@ -81,7 +81,7 @@ parser.add_argument('--task', type=str, default="Develop a basic Gomoku game.",
                     help="Prompt of software")
 parser.add_argument('--name', type=str, default="Gomoku",
                     help="Name of software, your software will be generated in WareHouse/name_org_timestamp")
-parser.add_argument('--model', type=str, default="GPT_3_5_TURBO",
+parser.add_argument('--model', type=str, default="qwen25_32b_instruct",
                     help="GPT Model, choose from {'GPT_3_5_TURBO', 'GPT_4', 'GPT_4_TURBO'}")
 parser.add_argument('--path', type=str, default="",
                     help="Your file directory, ChatDev will build upon your software in the Incremental mode")
